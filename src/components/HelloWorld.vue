29df48aad1b4efeb66b805e979e5c9c7018ed9e9
leblancr <rich.leblanc1@proton.me>
2024-08-09 23:02:06 -0400

    finished 3 Attribute Bindings

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,30 @@
-<script setup>
-import { reactive, ref } from 'vue'
+<script setup lang="ts">
+import { reactive, ref, defineProps } from 'vue'
 
+// Define the props interface
+interface Props {
+  msg: string
+}
+
+const props = defineProps<Props>()  // Use defineProps with the Props type
 const counter = reactive({ count: 0 })
 const message = ref('Hello World!')  // initial value
 console.log(counter.count) // "Hello World!"
 console.log(message.value) // 0
 counter.count = 1
 message.value = 'Changed'
+const titleClass = ref('title')
+
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
+  <h1 :class="titleClass">{{ message }}</h1>
   <p>Count is: {{ counter.count }}</p>
+  <span>Message: {{ props.msg }}</span>
 </template>
 
 <style scoped>
-
+.title {
+  color: red;
+}
 </style>
